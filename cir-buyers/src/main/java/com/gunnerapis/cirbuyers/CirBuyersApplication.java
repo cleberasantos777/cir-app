ad2b9c3caff02f3441756cfb0a8c72d4e70bafc6
Cleber Santos <clebersantos7@yahoo.com.br>
2022-01-31 19:19:22 -0300

    create project

---
+++ cir-buyers/src/main/java/com/gunnerapis/cirbuyers/CirBuyersApplication.java
@@ -0,0 +1,13 @@
+package com.gunnerapis.cirbuyers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirBuyersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirBuyersApplication.class, args);
+	}
+
+}
---
+++ cir-buyers/src/test/java/com/gunnerapis/cirbuyers/CirBuyersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunnerapis.cirbuyers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CirBuyersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
